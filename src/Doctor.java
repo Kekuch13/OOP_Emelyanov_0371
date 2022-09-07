cb8883429f0cf3bb6d11be8cb279e98378556646
Matvey <kassius2008@gmail.com>
2022-09-07 18:32:29 +0300

    Добавил поля

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,2 +1,6 @@
 public class Doctor {
+    private int id;
+    private String name;
+    private int age;
+    private String speciality;
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,7 @@
 public class Patient {
+    private int id;
+    private String name;
+    private String phone;
+    private String birthday;
+    private String contract;
 }
