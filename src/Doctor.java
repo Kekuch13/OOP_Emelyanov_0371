e3e3d5b2ea987f36c2a507a6b8ab6622980dfdfe
Matvey <kassius2008@gmail.com>
2022-09-11 20:32:37 +0300

    Update classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,6 +1,38 @@
 public class Doctor {
     private int id;
-    private String name;
     private int age;
+    private String name;
     private String speciality;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -4,4 +4,43 @@ public class Patient {
     private String phone;
     private String birthday;
     private String contract;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getContract() {
+        return contract;
+    }
+
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
 }
