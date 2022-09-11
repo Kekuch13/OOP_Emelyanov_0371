30be2eb2876371283bda3bd91ccd98ac0f458a1a
Matvey <kassius2008@gmail.com>
2022-09-11 20:46:27 +0300

    Update files

---
+++ src/Appointment.java
@@ -0,0 +1,38 @@
+public class Appointment {
+    private int id;
+    private String start;
+    private String end;
+    private int patientId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+}
---
+++ src/Day.java
@@ -0,0 +1,20 @@
+public class Day {
+    private String date;
+    private Appointment[] appointments;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Appointment[] getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Appointment[] appointments) {
+        this.appointments = appointments;
+    }
+}
